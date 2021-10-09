f79be4399ca841b73c43f11b273f2fca52d313ae
icompetetowin <79755401+icompetetowin@users.noreply.github.com>
2021-10-09 22:26:11 +0100

    Added conditions for api routes

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const filter = require("bad-words");
 const { customAlphabet } = require("nanoid");
-const alphabet = "0123456789";
-const nanoid = customAlphabet(alphabet, 6);
+const alphabet =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const nanoid = customAlphabet(alphabet, 10);
 const app = express();
 const port = 5000;
 
@@ -30,25 +31,28 @@ const Ranking = new mongoose.model("Ranking", rankingSchema);
 const customFilter = new filter({ placeHolder: "x" });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Good luck on FXC!" });
+  res.json({ message: "Good luck on FXC! :)" });
 });
 
 app.get("/api/highscores/:_id", async (req, res) => {
   try {
-    const top3 = await Ranking.find({}, "-_id nickname shortname highscore")
-      .sort({ highscore: -1, scoretime: 1 })
-      .limit(3);
     const player = await Ranking.findById(req.params._id);
-    const playersAbove = await Ranking.find({
-      highscore: { $gt: player.highscore }
-    }).count();
-    const personalRank = playersAbove + 1;
+    if (player) {
+      const top3 = await Ranking.find({}, "-_id nickname shortname highscore")
+        .sort({ highscore: -1, scoretime: 1 })
+        .limit(3);
 
-    res.json({
-      top3Arr: top3,
-      myRank: personalRank,
-      myHighscore: player.highscore
-    });
+      const playersAbove = await Ranking.find({
+        highscore: { $gt: player.highscore }
+      }).count();
+      const personalRank = playersAbove + 1;
+
+      res.json({
+        top3Arr: top3,
+        myRank: personalRank,
+        myHighscore: player.highscore
+      });
+    }
   } catch (error) {
     console.log(error);
   }
@@ -56,20 +60,22 @@ app.get("/api/highscores/:_id", async (req, res) => {
 
 app.post("/api/highscores", async (req, res) => {
   try {
-    const name = req.body.nickname;
-    const cleanName = customFilter.clean(name);
-    const regex = /[^0-9a-zA-Z]/g;
-    const filteredName = cleanName.replace(regex, "x");
-    const nameAndId = `${filteredName}#${nanoid()}`;
-    const myScoreTime = new Date().getTime();
-    const ranking = new Ranking({
-      nickname: nameAndId,
-      shortname: filteredName,
-      highscore: 0,
-      scoretime: myScoreTime
-    });
-    await ranking.save();
-    res.json(ranking);
+    if (req.body.nickname) {
+      const name = req.body.nickname;
+      const cleanName = customFilter.clean(name);
+      const regex = /[^0-9a-zA-Z]/g;
+      const filteredName = cleanName.replace(regex, "x");
+      const nameAndId = `${filteredName}#${nanoid()}`;
+      const myScoreTime = new Date().getTime();
+      const ranking = new Ranking({
+        nickname: nameAndId,
+        shortname: filteredName,
+        highscore: 0,
+        scoretime: myScoreTime
+      });
+      await ranking.save();
+      res.json(ranking);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -78,12 +84,14 @@ app.post("/api/highscores", async (req, res) => {
 app.put("/api/highscores/:_id", async (req, res) => {
   try {
     const ranking = await Ranking.findById(req.params._id);
-    const myScoreTime = new Date().getTime();
-    if (ranking.highscore < req.body.highscore) {
-      ranking.highscore = req.body.highscore;
-      ranking.scoretime = myScoreTime;
-      await ranking.save();
-      res.json(ranking);
+    if (ranking.nickname === req.body.nickname) {
+      const myScoreTime = new Date().getTime();
+      if (ranking.highscore < req.body.highscore) {
+        ranking.highscore = req.body.highscore;
+        ranking.scoretime = myScoreTime;
+        await ranking.save();
+        res.json(ranking);
+      }
     }
   } catch (error) {
     console.log(error);
